c631affb36611b005f021eec0b84b13c6d55dd53
Eugene Suhanov <suhanoves@gmail.com>
2021-08-31 21:01:53 +0300

    Сhange DEBUG setting

--- app/project/settings.py
+++ app/project/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
