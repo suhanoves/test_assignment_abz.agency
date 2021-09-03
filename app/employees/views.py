1d6512d163241892045575b3e768b227e1d49a00
Eugene Suhanov <suhanoves@gmail.com>
2021-09-03 19:32:54 +0300

    Add CompanyStructureView and EmployeesView

---
+++ app/employees/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import TemplateView, ListView
+from django.contrib.postgres.search import SearchVector
+from employees.models import Employee
+
+
+class CompanyStructureView(TemplateView):
+    template_name = 'employees/company_structure.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        company_structure = Employee.get_annotated_list()
+        context['company_structure'] = company_structure
+
+        return context
+
+
+class EmployeesView(ListView):
+    model = Employee
+    template_name = 'employees/employees.html'
+    paginate_by = 18
+    ordering = ['surname']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.annotate(
+                search=SearchVector('name', 'patronymic', 'surname', 'position', 'salary', 'start_date'),
+            ).filter(search=search)
+
+        return queryset
