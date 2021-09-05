2c03994cf413edd94dbdcccfefc7b13258e2fccf
Eugene Suhanov <suhanoves@gmail.com>
2021-09-05 21:29:00 +0300

    Add auth and signup url paths. Minor changes in other url paths

--- app/project/urls.py
+++ app/project/urls.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse_lazy('employees:company_structure'), permanent=True), name='homepage'),
     path('admin/', admin.site.urls),
-    path('', include('employees.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('', include('employees.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
