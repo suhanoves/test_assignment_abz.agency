dc7225f87a9eb065d791abb86fccc81112da147c
Eugene Suhanov <suhanoves@gmail.com>
2021-09-06 23:31:32 +0300

    Rename upload_to function for Employee

--- app/employees/models.py
+++ app/employees/models.py
@@ -3,15 +3,9 @@ from django.urls import reverse
 from treebeard.mp_tree import MP_Node
 
 
-def user_photo_directory_path(instance, filename):
-    """Generate path to user photo.
-
-    file will be uploaded to
-    MEDIA_ROOT/<first_char_of_surname>/<first_three_chars_of_surname>/<filename>
-    MEDIA_ROOT/<first_char_of_surname>/<first_three_chars_of_surname>/<filename>
-    """
-    name = instance.name.lower()
-    return f'{name[0]}/{name[:3]}/{filename}'
+def employees_photo_directory_path(instance, filename):
+    """Generate path to user photo."""
+    return f'employees_photos/{filename}'
 
 
 class Employee(MP_Node):
@@ -21,12 +15,18 @@ class Employee(MP_Node):
     position = models.CharField(max_length=200)
     start_date = models.DateField()
     salary = models.DecimalField(max_digits=19, decimal_places=2)
-    photo = models.ImageField(upload_to=user_photo_directory_path, null=True, blank=True)
+    photo = models.ImageField(upload_to=employees_photo_directory_path, null=True, blank=True)
 
     node_order_by = ['surname']
 
     def __str__(self):
         return f'{self.surname} {self.name} {self.patronymic}'
 
-    def get_full_name(self):
+    def get_fullname(self):
         return f'{self.name} {self.surname}'
+
+    def get_fullname_ru(self):
+        return f'{self.surname} {self.name} {self.patronymic}'
+
+    def get_absolute_url(self):
+        return reverse('employees:employee_detail', kwargs={'pk': self.pk})
